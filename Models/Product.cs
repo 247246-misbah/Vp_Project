7d5dc49bc89b87fedf698762bb4b3d37c8a984f4
247246-misbah <247246@students.au.edu.pk>
2026-05-16 23:03:41 +0500

    Initial Commit: Blazor Cafe Menu and Checkout Framework working perfectly

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,33 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
+﻿
 namespace Misbah_VisualProgramming_Project.Models
 {
-    [Table("Products")]
     public class Product
     {
-        [Key]
-        [Column("id")] // Maps uppercase 'Id' property to your database lowercase 'id' column
-        public int Id { get; set; }
-
-        [Required]
-        [Column("Name")]
+        public int ProductId { get; set; }
         public string Name { get; set; } = string.Empty;
-
-        [Column("Price")]
+        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
-
-        [Required]
-        [Column("Category")]
         public string Category { get; set; } = string.Empty;
-
-        [Column("IsAvailable")]
-        public bool IsAvailable { get; set; }
-
-        [Column("Stock")]
-        public int Stock { get; set; }
-
-        [Column("ImageUrl")]
-        public string ImageUrl { get; set; } = "https://images.unsplash.com/photo-1514432324607-a09d9b4aefdd?q=80&w=300";
+        public string DisplayPic { get; set; } = string.Empty;
     }
 }
--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d05026a70968ec0509b4dfe4f53c0d0abc1870")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d71fdb2b4c3523fe51dfd38fee9d15a79700c604")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
