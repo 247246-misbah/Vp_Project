9b6d860e58a66127f85301e183dee6e0f0ee889b
247246-misbah <247246@students.au.edu.pk>
2026-05-16 22:38:02 +0500

    Fix: Resolve routing query string conflict & synchronize high-fidelity image loaders on instant checkout mapping

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d581b03df19a37208372bfc3f3c7d3ccdcd04734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdc65823397da56d2dfb743f52156ccff518ea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
