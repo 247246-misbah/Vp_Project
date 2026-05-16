cd0d47d911a5814d0722d2fec54dc547cd4e3032
247246-misbah <247246@students.au.edu.pk>
2026-05-16 15:17:27 +0500

    Fix: Fully resolve syntax bracket alignment in AppDbContext infrastructure

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860b11f6104cfaf14344cda2295128a97983347d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+197aba1a7f1b1cd792447fdf818e3209454d031a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
