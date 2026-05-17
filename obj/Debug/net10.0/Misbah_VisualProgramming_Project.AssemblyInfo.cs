4b08be67f0e886f35a75464a1dfb6970cafdf80e
247246-misbah <247246@students.au.edu.pk>
2026-05-17 12:11:37 +0500

    style: bypass layout inheritance on login screen using direct absolute full-viewport block styling

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b90dac5c9826f2e43f05eab302a89f260fb06fc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e5ffca0c5d8e7ea5cb989c08fb796c71fa154d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
