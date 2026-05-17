225397b90c7c70774d23bc23db78718593321ced
247246-misbah <247246@students.au.edu.pk>
2026-05-17 12:25:58 +0500

    style: aggressively override layout sidebar on login page to ensure perfect center alignment

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dceec3b6a6615848cee5cb37353f727fd33239c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6e79239dcbf6b9e58b8c4406b6aa59991e24aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
