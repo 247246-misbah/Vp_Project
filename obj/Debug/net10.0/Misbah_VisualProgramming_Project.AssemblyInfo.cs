e18a0b0699374d2e1e4a7a02202c994d5d5e9b2c
247246-misbah <247246@students.au.edu.pk>
2026-05-17 21:18:18 +0500

    Fix Contact page escape keyframes css error

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e596052cecdb2edb59d9eb12cb3102e7b4c0ee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e4737d8b7a756346579d4befc7607bcfc3914c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
