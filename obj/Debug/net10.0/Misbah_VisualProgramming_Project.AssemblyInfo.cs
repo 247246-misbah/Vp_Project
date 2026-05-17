a13cc407fafe2787d843825f398c44e7d7d1a391
247246-misbah <247246@students.au.edu.pk>
2026-05-17 21:42:54 +0500

    Clean database schema, dropping obsolete tables while preserving core menu configurations

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ccabdd61a41d34f53f895c7b42fe0e8546f42e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9c4555ab9f6f4ff9d30420b31de56b40e3f2c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
