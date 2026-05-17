7e596052cecdb2edb59d9eb12cb3102e7b4c0ee3
247246-misbah <247246@students.au.edu.pk>
2026-05-17 21:15:14 +0500

    connecting contact .razor file to database

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f50770652268dc33ecb6d313d0b56f50814ee4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67ee94eebfe9dc8cc26da1f5720915a150ba6139")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
