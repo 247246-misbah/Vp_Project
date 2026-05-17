38fe622f255536a80304e74f7e50d0d334443ac9
247246-misbah <247246@students.au.edu.pk>
2026-05-17 17:49:31 +0500

    Fix: Restore original navigation logic methods and methods wrapper parameters in Contact view

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9521e989558568284c3fdb64ca8dfb76dba1fc3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31401865fd40a01be050a8c7265d043d80dcc5a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
