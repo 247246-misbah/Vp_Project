a00a8da0dcc0e64fee180e35e1de83b493e09cd7
247246-misbah <247246@students.au.edu.pk>
2026-05-17 13:13:35 +0500

    Changings updates in Dasboard.razor

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dd615748e68adcfca86397537ab60c5d7c172bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574eeae68ba1594e152d10ae92cfdd19d189f635")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
