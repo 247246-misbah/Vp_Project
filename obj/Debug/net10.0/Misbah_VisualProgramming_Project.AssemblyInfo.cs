c505acd9914f2c6af6dd0b156051f08ece67e7d9
247246-misbah <247246@students.au.edu.pk>
2026-05-17 22:04:06 +0500

    Establish clean orders database schema and bind dynamic live metrics onto admin dashboard

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13cc407fafe2787d843825f398c44e7d7d1a391")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d745a33b0fb6fc53cd439dc3f88660ac8c7d2ab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
