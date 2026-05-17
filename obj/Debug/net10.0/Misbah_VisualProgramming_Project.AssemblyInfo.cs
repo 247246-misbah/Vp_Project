ff4f7209f651a7d67bc622a83d53e2da6e55e428
247246-misbah <247246@students.au.edu.pk>
2026-05-17 22:32:46 +0500

    Fix syntax runtime bug on string expression encapsulation parameters for dynamic token rendering

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684d2b4cc1a187875343b46627ee02cd597c950d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09eb8d18377ff76343d5842283e6e37311255864")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
