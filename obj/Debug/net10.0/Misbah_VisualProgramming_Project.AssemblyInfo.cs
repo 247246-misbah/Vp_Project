d745a33b0fb6fc53cd439dc3f88660ac8c7d2ab1
247246-misbah <247246@students.au.edu.pk>
2026-05-17 21:57:38 +0500

    Fix sidebar navigation mapping to ensure Menu items route seamlessly to management page

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9c4555ab9f6f4ff9d30420b31de56b40e3f2c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13cc407fafe2787d843825f398c44e7d7d1a391")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
