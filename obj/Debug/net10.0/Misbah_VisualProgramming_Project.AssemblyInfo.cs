0d5c92921d42c6ad353861b36e8aeacf5d6dc8bd
247246-misbah <247246@students.au.edu.pk>
2026-05-20 23:23:43 +0500

    Final Release: Cafe Management System fully functional with dynamic live metrics

--- obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Misbah_VisualProgramming_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090c1db175f2c1b190f3bce08da5deb754a0f2cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97105c70d61fd78974a26bc2857284f402031f0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Misbah_VisualProgramming_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
